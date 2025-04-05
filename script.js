dc31b6c766873df7b842984e8507c060b6252fb4
RodriNA <aloha.rodrigo2013@gmail.com>
2025-04-05 21:12:33 +0000

    finished gambleling game

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+let textBox = document.getElementById("fname");
+let points = 0;
+let score = document.getElementById("points");
+
+
 function changeBackground(color) {
     let body = document.getElementById("body");
     body.style.backgroundColor = color;
     }
     
-    function randomColor() {
-        let colors = ["red", "orange", "yellow", "lime", "cyan", "navy blue", "dark purple", "pink"];
-        let randomColor = colors[Math.floor(Math.random() * colors.length)];
-        changeBackground(randomColor);
+function randomColor() {
+    let colors = ["red", "orange", "yellow", "lime", "cyan", "blue", "purple"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    changeBackground(randomColor);
+    if (textBox.value == randomColor) {
+        points = points + 1;
+        score.innerHTML = "points: " + points;
+    
     }
+    else {
+        points = points - 1;
+        score.innerHTML = "points:" + points;
+    }
+}
